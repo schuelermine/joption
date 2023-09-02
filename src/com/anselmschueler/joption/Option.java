acbe11cb0253148a30c461f09f51fcb632535b74
Anselm Schüler <mail@anselmschueler.com>
2023-09-02 13:18:17 +0200

    Add Iterable implementation & add a few methods

--- src/com/anselmschueler/joption/Option.java
+++ src/com/anselmschueler/joption/Option.java
@@ -1,13 +1,16 @@
 package com.anselmschueler.joption;
 
 import java.util.Objects;
+import java.util.Spliterator;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public sealed interface Option<T> permits Option.None, Option.Some {
+public sealed interface Option<T> extends Iterable<T> permits Option.None, Option.Some {
     public enum Never {}
     public enum Variant {
         SOME,
@@ -21,6 +24,8 @@ public sealed interface Option<T> permits Option.None, Option.Some {
             super(reason + " - attempt to get underlying value of Option variant None");
         }
     }
+    record Zip<T, U>(T left, U right) {}
+    record Unzip<T, U>(Option<T> left, Option<U> right) {}
 
     public T unwrap() throws NoneOptionException;
     public T unwrapOrNull();
@@ -52,6 +57,7 @@ public sealed interface Option<T> permits Option.None, Option.Some {
     public Option<T> xorCopying(Option<T> other);
     public Option<T> xorAndCopy(Option<? extends T> other);
     public Option<T> replace(T value);
+    public<U> Option<Zip<T, U>> zip(Option<U> other);
     public<U, V> Option<V> zipWith(BiFunction<? super T, U, V> function, Option<U> other);
 
     public static<T> Option<T> copy(Option<? extends T> other) {
@@ -61,9 +67,30 @@ public sealed interface Option<T> permits Option.None, Option.Some {
             return new Some<>(other.unwrapOrNull());
         }
     }
-
     public static<T> Option<T> flatten(Option<Option<T>> nested) {
         return nested.unwrapOrElse(() -> new None<>());
+    }
+    public static<T, U> Unzip<T, U> unzip(Option<Zip<T, U>> zipped) {
+        if (zipped.isSome()) {
+            var value = zipped.unwrapOrNull();
+            return new Unzip<>(new Some<>(value.left), new Some<>(value.right));
+        } else {
+            return new Unzip<>(new None<>(), new None<>());
+        }
+    }
+    public static<T> Option<T> getOrNewSome(Option<T> that, T value) {
+        if (that.isSome()) {
+            return that;
+        } else {
+            return new Some<>(value);
+        }
+    }
+    public static<T> Option<T> getOrNewSomeWith(Option<T> that, Supplier<T> value) {
+        if (that.isSome()) {
+            return that;
+        } else {
+            return new Some<T>(value.get());
+        }
     }
 
     public final class None<T> implements Option<T> {
@@ -71,12 +98,10 @@ public sealed interface Option<T> permits Option.None, Option.Some {
         public boolean equals(Object other) {
             return other instanceof None<?>;
         }
-
         @Override
         public int hashCode() {
             return Objects.hash();
         }
-
         @Override
         public String toString() {
             return "None[]";
@@ -184,8 +209,50 @@ public sealed interface Option<T> permits Option.None, Option.Some {
         public None<T> replace(T value) {
             return new None<>();
         }
+        public<U> Option<Zip<T, U>> zip(Option<U> other) {
+            return new None<>();
+        }
         public<U, V> None<V> zipWith(BiFunction<? super T, U, V> function, Option<U> other) {
             return new None<>();
+        }
+        public Iter iterator() {
+            return new Iter();
+        }
+        @Override
+        public Spliter spliterator() {
+            return new Spliter();
+        }
+
+        class Iter implements Iterator<T> {
+            public boolean hasNext() {
+                return false;
+            }
+            public T next() {
+                throw new NoSuchElementException();
+            }
+        }
+
+        class Spliter implements Spliterator<T> {
+            public int characteristics() {
+                return Spliterator.CONCURRENT
+                    | Spliterator.DISTINCT
+                    | Spliterator.SIZED
+                    | Spliterator.ORDERED
+                    | Spliterator.SUBSIZED;
+            }
+            public long estimateSize() {
+                return 0;
+            }
+            @Override
+            public long getExactSizeIfKnown() {
+                return 0;
+            }
+            public boolean tryAdvance(Consumer<? super T> action) {
+                return false;
+            }
+            public Spliter trySplit() {
+                return null;
+            }
         }
     }
     public final class Some<T> implements Option<T> {
@@ -207,12 +274,10 @@ public sealed interface Option<T> permits Option.None, Option.Some {
                 return false;
             }
         }
-
         @Override
         public int hashCode() {
             return Objects.hash(value);
         }
-
         @Override
         public String toString() {
             return "Some[value=" + value + "]";
@@ -327,15 +392,92 @@ public sealed interface Option<T> permits Option.None, Option.Some {
             }
         }
         public Some<T> replace(T value) {
-            T oldValue = this.value;
+            var oldValue = this.value;
             this.value = value;
             return new Some<>(oldValue);
+        }
+        public<U> Option<Zip<T, U>> zip(Option<U> other) {
+            if (other.isSome()) {
+                return new Some<>(new Zip<>(value, other.unwrapOrNull()));
+            } else {
+                return new None<>();
+            }
         }
         public<U, V> Option<V> zipWith(BiFunction<? super T, U, V> function, Option<U> other) {
             if (other.isSome()) {
                 return new Some<>(function.apply(value, other.unwrapOrNull()));
             } else {
                 return new None<>();
+            }
+        }
+        public Iter iterator() {
+            return new Iter(value);
+        }
+        @Override
+        public Spliter spliterator() {
+            return new Spliter(value);
+        }
+
+        class Iter implements Iterator<T> {
+            private T value;
+            private boolean done;
+
+            public Iter(T value) {
+                this.value = value;
+                done = false;
+            }
+
+            public boolean hasNext() {
+                return !done;
+            }
+            public T next() {
+                if (done) {
+                    throw new NoSuchElementException();
+                } else {
+                    done = true;
+                    return value;
+                }
+            }
+        }
+
+        class Spliter implements Spliterator<T> {
+            private T value;
+            private boolean done;
+
+            public Spliter(T value) {
+                this.value = value;
+                done = false;
+            }
+
+            public int characteristics() {
+                return Spliterator.CONCURRENT
+                    | Spliterator.DISTINCT
+                    | Spliterator.SIZED
+                    | Spliterator.ORDERED
+                    | Spliterator.SUBSIZED;
+            }
+            public long estimateSize() {
+                return done ? 0 : 1;
+            }
+            @Override
+            public long getExactSizeIfKnown() {
+                return done ? 0 : 1;
+            }
+            public boolean tryAdvance(Consumer<? super T> action) {
+                if (done) {
+                    return false;
+                } else {
+                    done = true;
+                    action.accept(value);
+                    return true;
+                }
+            }
+            public Spliter trySplit() {
+                if (done) {
+                    return null;
+                }
+                done = true;
+                return new Spliter(value);
             }
         }
     }
